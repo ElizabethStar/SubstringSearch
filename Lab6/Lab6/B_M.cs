5e2bb4526b4f0708c39a52d0c1535115ea05e707
Елизавета Старикова <starikovaliza02@gmail.com>
2023-05-15 21:30:18 +0300

    надеюсь последняя версия

--- Lab6/Lab6/B_M.cs
+++ Lab6/Lab6/B_M.cs
@@ -6,46 +6,34 @@ using System.Threading.Tasks;
 
 namespace Lab6
 {
-    public class B_M: ISubstringSearch
+    public class B_M : ISubstringSearch
     {
-        public void preprocess_strong_suffix(int[] shift, int[] bpos,
-                                   char[] pat, int m)
+
+        public void preprocess_strong_suffix(int[] shift, int[] borderPositions, char[] p, int m)
         {
-
             int i = m, j = m + 1;
-            bpos[i] = j;
-
+            borderPositions[i] = j;
             while (i > 0)
             {
-
-                while (j <= m && pat[i - 1] != pat[j - 1])
+                while (j <= m && p[i - 1] != p[j - 1])
                 {
-
-                    if (shift[j] == 0)
-                        shift[j] = j - i;
-
-
-                    j = bpos[j];
+                    if (shift[j] == 0) shift[j] = j - i;
+                    j = borderPositions[j];
                 }
-
                 i--; j--;
-                bpos[i] = j;
+                borderPositions[i] = j;
             }
         }
 
 
-        public void preprocess_case2(int[] shift, int[] bpos,
-                                    char[] pat, int m)
+        public void preprocess_case2(int[] shift, int[] borderPositions, int m)
         {
             int i, j;
-            j = bpos[0];
+            j = borderPositions[0];
             for (i = 0; i <= m; i++)
             {
-                if (shift[i] == 0)
-                    shift[i] = j;
-
-                if (i == j)
-                    j = bpos[j];
+                if (shift[i] == 0) shift[i] = j;
+                if (i == j) j = borderPositions[j];
             }
         }
         public List<int> SubstringSearch(string pat, string text)
@@ -55,14 +43,14 @@ namespace Lab6
             int m = pat.Length;
             int n = text.Length;
 
-            int[] bpos = new int[m + 1];
+            int[] borderPositions = new int[m + 1];
             int[] shift = new int[m + 1];
 
             for (int i = 0; i < m + 1; i++)
                 shift[i] = 0;
 
-            preprocess_strong_suffix(shift, bpos, pat.ToArray(), m);
-            preprocess_case2(shift, bpos, pat.ToArray(), m);
+            preprocess_strong_suffix(shift, borderPositions, pat.ToArray(), m);
+            preprocess_case2(shift, borderPositions, m);
 
             while (s <= n - m)
             {
@@ -80,6 +68,86 @@ namespace Lab6
                     s += shift[j + 1];
             }
             return list;
+
         }
+
+
+
     }
+    //public class B_M: ISubstringSearch
+    //{
+    //    public void preprocess_strong_suffix(int[] shift, int[] bpos,
+    //                               char[] pat, int m)
+    //    {
+
+    //        int i = m, j = m + 1;
+    //        bpos[i] = j;
+
+    //        while (i > 0)
+    //        {
+
+    //            while (j <= m && pat[i - 1] != pat[j - 1])
+    //            {
+
+    //                if (shift[j] == 0)
+    //                    shift[j] = j - i;
+
+
+    //                j = bpos[j];
+    //            }
+
+    //            i--; j--;
+    //            bpos[i] = j;
+    //        }
+    //    }
+
+
+    //    public void preprocess_case2(int[] shift, int[] bpos,
+    //                                char[] pat, int m)
+    //    {
+    //        int i, j;
+    //        j = bpos[0];
+    //        for (i = 0; i <= m; i++)
+    //        {
+    //            if (shift[i] == 0)
+    //                shift[i] = j;
+
+    //            if (i == j)
+    //                j = bpos[j];
+    //        }
+    //    }
+    //    public List<int> SubstringSearch(string pat, string text)
+    //    {
+    //        List<int> list = new List<int>();
+    //        int s = 0, j;
+    //        int m = pat.Length;
+    //        int n = text.Length;
+
+    //        int[] bpos = new int[m + 1];
+    //        int[] shift = new int[m + 1];
+
+    //        for (int i = 0; i < m + 1; i++)
+    //            shift[i] = 0;
+
+    //        preprocess_strong_suffix(shift, bpos, pat.ToArray(), m);
+    //        preprocess_case2(shift, bpos, pat.ToArray(), m);
+
+    //        while (s <= n - m)
+    //        {
+    //            j = m - 1;
+
+    //            while (j >= 0 && pat[j] == text[s + j])
+    //                j--;
+
+    //            if (j < 0)
+    //            {
+    //                list.Add(s);
+    //                s += shift[0];
+    //            }
+    //            else
+    //                s += shift[j + 1];
+    //        }
+    //        return list;
+    //    }
+    //}
 }
--- Lab6/Lab6/Brute_force.cs
+++ Lab6/Lab6/Brute_force.cs
@@ -8,32 +8,18 @@ namespace Lab6
 {
     public class Brute_force: ISubstringSearch
     {
-        public List<int> SubstringSearch( string match, string text)
+        public List<int> SubstringSearch(string match, string text)
         {
             List<int> answer = new List<int>();
             //int textLength = text.Length;
             //int matchLenght = match.Length;
             for (int i = 0; i <= text.Length - match.Length; i++)
             {
-                bool OK = true;
-                for (int j = 0; j < match.Length; j++)
-                {
-                    string textChar = text.Substring(i + j, 1);
-                    string matchChar = match.Substring(j, 1);
-                    if (matchChar != textChar)
-                    {
-                        OK = false;
-                        break;
+                //bool OK = true;
 
-                    }
-                }
-                //string textChar = text.Substring(i , match.Length);
-                //if (textChar!=match)
-                //{
-                //    OK = false;
-                //}
-                if (OK != false)
-                {
+                string textpart = text.Substring(i, match.Length);
+                if (textpart == match)
+                {                 
                     answer.Add(i);
                 }
             }
@@ -42,3 +28,23 @@ namespace Lab6
         }
     }
 }
+
+
+
+
+
+
+
+
+
+//for (int j = 0; j < match.Length; j++)
+//{
+//    string textChar = text.Substring(i + j, 1);
+//    string matchChar = match.Substring(j, 1);
+//    if (matchChar != textChar)
+//    {
+//        OK = false;
+//        break;
+
+//    }
+//}
